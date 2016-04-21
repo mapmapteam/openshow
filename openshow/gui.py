5de31aa476e4f218c4b3d0f96c32d7ef34881bd3
Alexandre Quessy <alexandre@quessy.net>
2016-04-21 12:23:35 -0400

    add an open file menu item

--- openshow/gui.py
+++ openshow/gui.py
@@ -6,24 +6,52 @@ import sys
 import wx
 # At this point, wxreactor has already been installed.
 from twisted.internet import reactor
+import os
+
+def show_open_file_dialog(parent):
+    """
+    Open a file.
+    """
+    dirname = ''
+    filename = ''
+    full_path = ''
+    dialog = wx.FileDialog(parent, "Choose a file", dirname, "", "*.xml",
+            wx.OPEN)
+    if dialog.ShowModal() == wx.ID_OK:
+        filename = dialog.GetFilename()
+        dirname = dialog.GetDirectory()
+        full_path = os.path.join(dirname, filename)
+    dialog.Destroy()
+    return full_path
 
 
 class MainFrame(wx.Frame):
     def __init__(self, parent, ID, title):
-        ID_EXIT  = 101
-        wx.Frame.__init__(self, parent, ID, title, wx.DefaultPosition, wx.Size(300, 200))
-        menu = wx.Menu()
-        menu.Append(ID_EXIT, "E&xit", "Terminate the program")
+        # ID_EXIT = wx.NewId() # 101
+        wx.Frame.__init__(self, parent, ID, title, wx.DefaultPosition,
+                wx.Size(720, 480))
         menuBar = wx.MenuBar()
-        menuBar.Append(menu, "&File")
+        # File
+        file_menu = wx.Menu()
+        # File > Open
+        file_menu.Append(wx.ID_OPEN, "&Open", "Open a project file")
+        wx.EVT_MENU(self, wx.ID_OPEN, self._open_menu_cb)
+        # File > Exit
+        file_menu.Append(wx.ID_EXIT, "E&xit", "Exit the program")
+        wx.EVT_MENU(self, wx.ID_EXIT, self._exit_menu_cb)
+
+        menuBar.Append(file_menu, "&File")
         self.SetMenuBar(menuBar)
-        wx.EVT_MENU(self, ID_EXIT,  self._exit_menu_cb)
         
         # make sure reactor.stop() is used to stop event loop:
         wx.EVT_CLOSE(self, lambda evt: reactor.stop())
 
     def _exit_menu_cb(self, event):
         reactor.stop()
+
+    def _open_menu_cb(self, event):
+        file_path = show_open_file_dialog(self)
+        print("Chose file %s" % (file_path))
 
 
 class App(wx.App):
