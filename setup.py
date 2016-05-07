69848d8f936de146a7c00dd11694f7ce3b4a34b6
Alexandre Quessy <alexandre@quessy.net>
2016-05-07 12:21:04 +0000

    update list of authors

--- setup.py
+++ setup.py
@@ -12,16 +12,16 @@ from twisted.python import procutils
 setup(
     name = "openshow",
     version = openshow.__version__,
-    author = "Alexandre Quessy",
+    author = "Alexandre Quessy, Sofian Audry, Dame Diongue",
     author_email = "alexandre@quessy.net",
     url = "http://github.com/mapmapteam/openshow",
     description = "Open Show Control",
     scripts = [
         "scripts/openshow", 
         ],
-    license="MIT/X",
+    license="LGPL",
     packages = ["openshow", "openshow/test"],
-    long_description = """Open Show Control is a cue sheet editor for stage managing.""",
+    long_description = """OpenShow is a show control app to trigger theatrical cues at a specific time..""",
     classifiers = [
         "Framework :: Twisted",
         "Intended Audience :: Developers",
@@ -35,7 +35,7 @@ setup(
 
 if sys.argv[1] == "build":
     commands = [
-        'help2man --no-info --include=man-openshow.txt --no-discard-stderr --name="Open Show Control" ./scripts/openshow --output=openshow.1',
+        'help2man --no-info --include=man-openshow.txt --no-discard-stderr --name="OpenShow" ./scripts/openshow --output=openshow.1',
         ]
     if os.path.exists("man-openshow.txt"):
         try:
